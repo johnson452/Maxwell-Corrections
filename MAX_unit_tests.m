9c93fe018f52994c0b9e2a1720548317464dae5e
johnson452 <grj@princeton.edu>
2024-03-06 16:16:07 -0500

    minor cleanup

--- MAX_unit_tests.m
+++ MAX_unit_tests.m
@@ -12,26 +12,25 @@ close all
 
 
 %%% WENO TESTING %%%
-grid.moments_type = "Simple_No_Weno_reconst_fv";
-grid.Nx = 1;
-grid.Nv = 10; %150;
-Nx = grid.Nx;
-Nv = grid.Nv;
+grid_struct.moments_type = "Simple_No_Weno_reconst_fv";
+grid_struct.Nx = 1;
+grid_struct.Nv = 10; %150;
+Nx = grid_struct.Nx;
+Nv = grid_struct.Nv;
 f_IC = zeros(Nx,Nv);
-f_fixed = zeros(Nx,Nv);
-grid.x_max = 1;
-grid.x_min = 0;
-grid.Lx = grid.x_max - grid.x_min;
-grid.x = linspace(grid.x_min,grid.x_max,grid.Nx);
+grid_struct.x_max = 1;
+grid_struct.x_min = 0;
+grid_struct.Lx = grid_struct.x_max - grid_struct.x_min;
+grid_struct.x = linspace(grid_struct.x_min,grid_struct.x_max,grid_struct.Nx);
 
-grid.v_max = 4;
-grid.v_min = -grid.v_max;
-grid.Lv = grid.v_max - grid.v_min;
-grid.v = linspace(grid.v_min,grid.v_max,grid.Nv);
-grid.dv = grid.v(2) - grid.v(1);
+grid_struct.v_max = 4;
+grid_struct.v_min = -grid_struct.v_max;
+grid_struct.Lv = grid_struct.v_max - grid_struct.v_min;
+grid_struct.v = linspace(grid_struct.v_min,grid_struct.v_max,grid_struct.Nv);
+grid_struct.dv = grid_struct.v(2) - grid_struct.v(1);
 
-% Save the grid to an app
-app.grid = grid;
+% Save the grid_struct.to an app
+app.grid_struct = grid_struct;
 
 % Max testing
 app.m0 = 1;
@@ -43,8 +42,8 @@ u0 = 1; %1;
 T0 = 0.3; %1;
 
 % Make a maxwellian distribtuion
-for i = 1:grid.Nv
-    f_IC(i) = maxwellian(n0,u0,T0,grid.v(i),app);
+for i = 1:grid_struct.Nv
+    f_IC(i) = maxwellian(n0,u0,T0,grid_struct.v(i),app);
 end
 
 % Fake t for ref
@@ -54,17 +53,17 @@ ts = [0.25,0.75];
 % Fix the maxwellian
 fprintf("First correction:\n")
 tic
-f_fixed = fix_max(f_IC,n0,u0,T0,app);
+f_fixed_Picard = fix_max(f_IC,n0,u0,T0,app);
 toc
 
 fprintf("\n\nSecond correction:\n")
 tic
-f_fixed = fix_max_2(f_IC,n0,u0,T0,app);
+f_fixed_Newton = fix_max_2(f_IC,n0,u0,T0,app);
 toc
 
 % Initial projection/fixed
 [n1,u1,T1] = moments(f_IC,app);
-[n2,u2,T2] = moments(f_fixed,app);
+[n2,u2,T2] = moments(f_fixed_Newton,app);
 n_corr = [n1,n2];
 u_corr = [u1,u2];
 T_corr = [T1,T2];
@@ -72,9 +71,9 @@ T_corr = [T1,T2];
 
 % Plot the exact solution
 subplot(2,3,1)
-plot(grid.v,f_IC,"-*blue",LineWidth=2)
+plot(grid_struct.v,f_IC,"-*blue",LineWidth=2)
 hold on
-plot(grid.v,f_fixed,":",LineWidth=2)
+plot(grid_struct.v,f_fixed_Newton,":",LineWidth=2)
 title("f(v)")
 xlabel("v")
 ylabel("f(v)")
@@ -113,8 +112,8 @@ legend("Specified","Fixed")
 
 % Plot the moments
 subplot(2,3,5)
-plot(grid.v,f_IC - f_fixed,LineWidth=2)
+plot(grid_struct.v,f_IC - f_fixed_Newton,LineWidth=2)
 title("f(v) [IC - fixed]")
 xlabel("v")
 ylabel("f(v)")
-grid on
+grid_struct.on
